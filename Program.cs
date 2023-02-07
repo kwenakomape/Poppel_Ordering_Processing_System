55018af51ecea6eee82023dd13b0366c79e9f17d
kwenakomape <81844281+kwenakomape@users.noreply.github.com>
2023-02-07 16:44:00 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Poppel_Order_Processing_System.PresentationLayer;
+// ReSharper disable All
+
+namespace Poppel_Order_Processing_System
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///     The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        private static void Main()
+        {
+
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PoppelOrderProcessingSystem());
+        }
+    }
+}
